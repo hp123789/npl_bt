5ac92c99a7f745ec7702ae97eb1a1ab552078f43
Hamza <hamzaperacha@gmail.com>
2024-03-04 11:16:33 -0800

    load supergraph

--- mouse/mouse_emulate.py
+++ mouse/mouse_emulate.py
@@ -8,6 +8,7 @@ import dbus.mainloop.glib
 import redis
 import numpy as np
 import time
+import json
 
 class MouseClient():
 
@@ -23,14 +24,51 @@ class MouseClient():
 			self.iface.send_mouse(0, bytes(self.state))
 		except OSError as err:
 			error(err)
+
+	def load_supergraph(self):
+		supergraph_entries = self.redis_conn.xrevrange("supergraph_stream", count=1)
+
+		# Parse the result from redis.
+		supergraph_id, supergraph_entry = supergraph_entries[0]
+		supergraph_bytes = supergraph_entry[b"data"]
+		supergraph_str = supergraph_bytes.decode()
+		supergraph_dict = json.loads(supergraph_str)
+
+		# If this is a new supergraph, update this class's version.
+		if supergraph_id != self.supergraph_id:
+			self.supergraph_id = supergraph_id
+			self.supergraph_dict = supergraph_dict
+			# Also grab the parameters for this specific node.
+			matching_node_dicts = [
+				n
+				for n in supergraph_dict["nodes"].values()
+				if n["nickname"] == "brainToText_personalUse"
+			]
+			if not matching_node_dicts:
+				message = {"message": f"Bluetooth: No parameters entry in supergraph for node '{self.nickname}'"}
+				self.r.xadd("console_logging", message)
+			node_dict = matching_node_dicts[0]
+
+			node_params = node_dict["parameters"]
+
+			if node_params.get('run_mouse') is None:
+				return None
+			else:
+				run_mouse = np.array(node_params['run_mouse'])
+				run_click = np.array(node_params['run_click'])
+				screen_height = np.array(node_params['screen_height'])
+				return run_mouse, run_click, screen_height
 	
 	def run(self):
 		
 		self.input_stream = "cursor_2d_commands"
 		self.discrete_input_stream = "decoded_gestures"
 		self.last_input_entry_seen = "$"
-		self.screen_height = 1964
 		self.r = redis.Redis('192.168.150.2')
+
+		self.run_mouse = True
+		self.run_click = True
+		self.screen_height = 1964
 
 		last_input_entries = self.r.xrevrange(self.input_stream, count=1)
 		self.last_input_entry_seen = (
@@ -47,67 +85,78 @@ class MouseClient():
 		)
 	
 		while True:
+
+			if self.load_supergraph():
+				self.run_mouse, self.run_click, self.screen_height = self.load_supergraph()
+			else:
+				self.run_mouse = True
+				self.run_click = True
+				self.screen_height = 1964
+
+			if self.run_mouse:
 			
-			read_result = self.r.xread(
-                    {
-                        self.input_stream: "$",
-						self.discrete_input_stream: last_discrete_input_entry_seen,
-                    }, count=1, block=0
-                )
+				read_result = self.r.xread(
+						{
+							# replace "$" with self.last_input_entry_seen, but gets bogged down
+							self.input_stream: "$",
+							self.discrete_input_stream: last_discrete_input_entry_seen,
+						}, count=1, block=0
+					)
 
-			read_result_dict = {
-				stream.decode(): entries for stream, entries in read_result
-			}
+				read_result_dict = {
+					stream.decode(): entries for stream, entries in read_result
+				}
 
-			for input_entry_id, input_entry_dict in read_result_dict.get(
-				self.input_stream, []
-			):
-				# Save that we've now seen this entry.
-				self.last_input_entry_seen = input_entry_id
+				for input_entry_id, input_entry_dict in read_result_dict.get(
+					self.input_stream, []
+				):
+					# Save that we've now seen this entry.
+					self.last_input_entry_seen = input_entry_id
 
-				# Input command received.
-				distance_bgcoordinates = np.frombuffer(
-					input_entry_dict[b"data"], dtype=np.float32
-				)
-				x_bgcoordinates = distance_bgcoordinates[0]
-				y_bgcoordinates = distance_bgcoordinates[1]
+					# Input command received.
+					distance_bgcoordinates = np.frombuffer(
+						input_entry_dict[b"data"], dtype=np.float32
+					)
+					x_bgcoordinates = distance_bgcoordinates[0]
+					y_bgcoordinates = distance_bgcoordinates[1]
 
-				x_final = int(x_bgcoordinates * self.screen_height)
-				y_final = int(y_bgcoordinates * self.screen_height)
+					x_final = int(x_bgcoordinates * self.screen_height)
+					y_final = int(y_bgcoordinates * self.screen_height)
 
-				if (x_final < 0):
-					x_final = 256 + x_final
+					if (x_final < 0):
+						x_final = 256 + x_final
 
-				if (y_final > 0):
-					y_final = 256 - y_final
+					if (y_final > 0):
+						y_final = 256 - y_final
 
-				if (y_final < 0):
-					y_final = -1*y_final
+					if (y_final < 0):
+						y_final = -1*y_final
 
-				#print(x_final,y_final)
+					#print(x_final,y_final)
 
-				self.state[1] = int(x_final)
-				self.state[2] = int(y_final)
+					self.state[1] = int(x_final)
+					self.state[2] = int(y_final)
 
-			for (
-				discrete_input_entry_id,
-				discrete_input_entry_dict,
-			) in read_result_dict.get(self.discrete_input_stream, []):
-				# Save that we've now seen this entry.
-				last_discrete_input_entry_seen = discrete_input_entry_id
+				if self.run_click:
 
-				# Discrete action command received.
-				output_class = discrete_input_entry_dict[b"output_class"].decode()
+					for (
+						discrete_input_entry_id,
+						discrete_input_entry_dict,
+					) in read_result_dict.get(self.discrete_input_stream, []):
+						# Save that we've now seen this entry.
+						last_discrete_input_entry_seen = discrete_input_entry_id
 
-				# Ignore it if it is the null action.
-				if output_class != "no_action":
-					self.state[0] = 1
+						# Discrete action command received.
+						output_class = discrete_input_entry_dict[b"output_class"].decode()
+
+						# Ignore it if it is the null action.
+						if output_class != "no_action":
+							self.state[0] = 1
+
+				self.send_current()
+				self.state[0] = 0
 				
-			self.send_current()
-			
-			self.state[0] = 0
-
-			time.sleep(0.01)
+				time.sleep(0.01)
 
 
 if __name__ == "__main__":
