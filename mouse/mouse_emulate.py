dbaf2861973baed1d7697c25f017e4a418fcc708
Hamza <hamzaperacha@gmail.com>
2024-03-11 16:32:30 -0700

    vhidufj

--- mouse/mouse_emulate.py
+++ mouse/mouse_emulate.py
@@ -27,7 +27,7 @@ class MouseClient():
 	def send_current(self):
 		try:
 			self.iface.send_mouse(0, bytes(self.state))
-			# self.r.xadd("bluetooth_cursor_commands", {"state": str(self.state)})
+			self.r.xadd("bluetooth_cursor_commands", {"state": str(self.state)})
 		except OSError as err:
 			error(err)
 
@@ -104,7 +104,7 @@ class MouseClient():
 				read_result = self.r.xread(
 						{
 							# replace "$" with self.last_input_entry_seen, but gets bogged down
-							self.input_stream: "$",
+							self.input_stream: self.last_input_entry_seen,
 							self.discrete_input_stream: self.last_discrete_input_entry_seen,
 						}, block=0
 					)
@@ -133,31 +133,8 @@ class MouseClient():
 					x = int(x_bgcoordinates * self.bluetooth_px_per_bgunit)
 					y = int(y_bgcoordinates * self.bluetooth_px_per_bgunit)
 
-					x_final = x
-					y_final = y
-
-					# if not disable_bluetooth_cursor:
-					if (x_final < 0):
-						x_final = 255 + x_final
-
-					if (y_final > 0):
-						y_final = 255 - y_final
-
-					if (y_final < 0):
-						y_final = -1*y_final
-					
-
-					if x_final < 0: x_final = 0
-					if x_final > 255: x_final = 255
-					
-					if y_final < 0: y_final = 0
-					if y_final > 255: y_final = 255
-
-
-					self.state[1] = int(x_final)
-					self.state[2] = int(y_final)
-
-					self.send_current()
+					x_final += x
+					y_final += y
 
 					#print(x_final,y_final)
 
@@ -191,7 +168,28 @@ class MouseClient():
 						self.state[0] = 0
 						self.send_current()
 
-				
+				if not disable_bluetooth_cursor:
+					if (x_final < 0):
+						x_final = 255 + x_final
+
+					if (y_final > 0):
+						y_final = 255 - y_final
+
+					if (y_final < 0):
+						y_final = -1*y_final
+					
+
+					if x_final < 0: x_final = 0
+					if x_final > 255: x_final = 255
+					
+					if y_final < 0: y_final = 0
+					if y_final > 255: y_final = 255
+
+
+					self.state[1] = int(x_final)
+					self.state[2] = int(y_final)
+
+					self.send_current()
 				
 				# time.sleep(0.01)
 					
